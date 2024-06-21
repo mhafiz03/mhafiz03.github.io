843455851fc1f88c760a8dcad46ae3e265825e2f
mhafiz03 <mhafiz@student.telkomuniversity.ac.id>
2024-06-21 07:26:24 +0700

    First commit

---
+++ _next/static/chunks/app/not-found-e4f17161b802bd60.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[160,931],{7060:function(e,n,s){Promise.resolve().then(s.t.bind(s,3989,23)),Promise.resolve().then(s.t.bind(s,1102,23))}},function(e){e.O(0,[102,989,192,221,744],function(){return e(e.s=7060)}),_N_E=e.O()}]);
---
+++ _next/static/chunks/app/page-02d4c022f5fc1b79.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931,160],{7060:function(e,n,s){Promise.resolve().then(s.t.bind(s,3989,23)),Promise.resolve().then(s.t.bind(s,1102,23))}},function(e){e.O(0,[102,989,192,221,744],function(){return e(e.s=7060)}),_N_E=e.O()}]);
